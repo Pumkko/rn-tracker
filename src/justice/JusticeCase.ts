189cacb945c6de0eef34ccdc97c9f42d56abf9d3
Pumkko <charles.coque@gmail.com>
2024-06-18 11:51:26 -0400

    Update src/justice/JusticeCase.ts
    Co-authored-by: Sylvain Philippe <sylvain-philippe@club-internet.fr>

--- src/justice/JusticeCase.ts
+++ src/justice/JusticeCase.ts
@@ -2,6 +2,7 @@ import type { JusticeCaseSource } from "./JusticeCaseSource";
 
 export type JusticeCase = {
     state: 'En Cours' | 'Condamné',
+    name: string;
     description: string;
     source: JusticeCaseSource[]
 }
