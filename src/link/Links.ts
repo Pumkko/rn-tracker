e712dc094cfe91627646151c7de1d415a6be3c1d
Sylvain Philippe <sylvain.philippe@invisart.fr>
2024-06-28 14:34:27 +0200

    [src][link] Add RessourceLink, its card and a page displaying links

---
+++ src/link/Links.ts
@@ -0,0 +1,25 @@
+import type {RessourceLink} from './RessourceLink';
+
+const Links: RessourceLink[] =
+    [
+      {
+
+        name: '120 preuves que le RN est toujours ...',
+        sourceUrl: 'https://cartedelahonte.github.io/'
+
+      },
+      {
+
+        name: 'Le comparateur de programme',
+        sourceUrl: 'https://comparateur.nosservicespublics.fr/'
+
+      },
+      {
+
+        name: 'Les votes du groupe Rassemblement National',
+        sourceUrl: 'https://datan.fr/groupes/legislature-16/rn/votes'
+
+      },
+    ]
+
+    export default Links;
---
+++ src/link/RessourceLink.ts
@@ -0,0 +1,4 @@
+
+export type RessourceLink = {
+  name: string; sourceUrl: string;
+}
