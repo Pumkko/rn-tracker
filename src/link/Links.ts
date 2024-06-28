67d41e7b83eb910edf8534e99f5a3a95d9ac6f92
Sylvain Philippe <sylvain.philippe@invisart.fr>
2024-06-28 14:56:27 +0200

    [src][link] Add imageUrl for link previews

--- src/link/Links.ts
+++ src/link/Links.ts
@@ -5,19 +5,22 @@ const Links: RessourceLink[] =
       {
 
         name: '120 preuves que le RN est toujours ...',
-        sourceUrl: 'https://cartedelahonte.github.io/'
+        sourceUrl: 'https://cartedelahonte.github.io/',
+        imageUrl: 'https://cartedelahonte.github.io/images/social-card.png'
 
       },
       {
 
         name: 'Le comparateur de programme',
-        sourceUrl: 'https://comparateur.nosservicespublics.fr/'
+        sourceUrl: 'https://comparateur.nosservicespublics.fr/',
+        imageUrl: 'https://comparateur.nosservicespublics.fr/img/nsp-logo.svg'
 
       },
       {
 
         name: 'Les votes du groupe Rassemblement National',
-        sourceUrl: 'https://datan.fr/groupes/legislature-16/rn/votes'
+        sourceUrl: 'https://datan.fr/groupes/legislature-16/rn/votes',
+        imageUrl: 'https://datan.fr/assets/imgs/groupes/16/webp/RN.webp'
 
       },
     ]
