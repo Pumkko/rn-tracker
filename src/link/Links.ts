82ee8277b0304ba92e8905a8235024241661be9f
Sylvain Philippe <sylvain.philippe@invisart.fr>
2024-07-02 12:02:26 +0200

    [src][links] Display X, tiktok and youtube embed, add 2 links

--- src/link/Links.ts
+++ src/link/Links.ts
@@ -6,23 +6,48 @@ const Links: RessourceLink[] =
 
         name: '120 preuves que le RN est toujours ...',
         sourceUrl: 'https://cartedelahonte.github.io/',
-        imageUrl: 'https://cartedelahonte.github.io/images/social-card.png'
-
+        imageUrl: 'https://cartedelahonte.github.io/images/social-card.png',
+        tweetUrl: '',
+        tiktokUrl: '',
+        youtubeUrl: ''
       },
       {
 
         name: 'Le comparateur de programme',
         sourceUrl: 'https://comparateur.nosservicespublics.fr/',
-        imageUrl: 'https://comparateur.nosservicespublics.fr/img/nsp-logo.svg'
-
+        imageUrl: 'https://comparateur.nosservicespublics.fr/img/nsp-logo.svg',
+        tweetUrl: '',
+        tiktokUrl: '',
+        youtubeUrl: ''
+      },
+      {
+        name: 'Les votes du groupe Rassemblement National',
+        sourceUrl: 'https://datan.fr/groupes/legislature-16/rn/votes',
+        imageUrl: 'https://datan.fr/assets/imgs/groupes/16/webp/RN.webp',
+        tweetUrl: '',
+        tiktokUrl: '',
+        youtubeUrl: ''
       },
       {
 
-        name: 'Les votes du groupe Rassemblement National',
-        sourceUrl: 'https://datan.fr/groupes/legislature-16/rn/votes',
-        imageUrl: 'https://datan.fr/assets/imgs/groupes/16/webp/RN.webp'
-
+        name: 'Nullité des candidats RN',
+        sourceUrl: 'https://x.com/barriere_dr/status/1807844666470957533',
+        imageUrl: '',
+        tweetUrl: 'https://x.com/barriere_dr/status/1807844666470957533',
+        tiktokUrl: '',
+        youtubeUrl: ''
       },
+      {
+
+        name: 'La réalité sur les votes RN ',
+        sourceUrl:
+            'https://www.tiktok.com/@malopedia/video/7381626694530108704',
+        imageUrl: '',
+        tweetUrl: '',
+        tiktokUrl:
+            'https://www.tiktok.com/@malopedia/video/7381626694530108704',
+        youtubeUrl: ''
+      }
     ]
 
     export default Links;
